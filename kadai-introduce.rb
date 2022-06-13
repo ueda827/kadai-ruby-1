8792ef419b18259c1a964ecff8dfb2cac91c344c
s.u <shota.827@gmail.com>
2022-06-13 06:42:36 +0000

    add kadai-introduce.rb

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,8 +1,8 @@
 name = "翔太"
 last_name = "上田"
-age = 32 #数字はベタうちのがいいのか？
+age = 32 
 full_name = last_name + name
 str1 = "です。"
 str2 = "歳です。"
 
-puts full_name + str1 + 32.to_s + str2
+puts full_name + str1 + age.to_s + str2
